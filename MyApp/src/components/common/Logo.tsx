29efbf9fb938a91f964bfb5f86c18ec56bc99407
Tanishy <mambaout134@gmail.com>
2021-02-03 22:43:33 +0900

    Fix eslint errors

--- MyApp/src/components/common/Logo.tsx
+++ MyApp/src/components/common/Logo.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Image, ImageSourcePropType } from 'react-native';
+import { Image, ImageSourcePropType, ImageStyle, StyleProp } from 'react-native';
 
 const logo: ImageSourcePropType = require('../../assets/Los_Angeles_Lakers_logo.png');
 
-export default function Logo() {
-  return <Image source={logo} />;
+export default function Logo(props: { style?: StyleProp<ImageStyle> }) {
+  const { style } = props;
+  return <Image source={logo} style={style} />;
 }
--- MyApp/src/models/Roster.ts
+++ MyApp/src/models/Roster.ts
@@ -1,8 +1,8 @@
-import Coach from "./Coach";
-import Player from "./Player";
+import Coach from './Coach';
+import Player from './Player';
 
 export default interface Roster {
-  teamName: string,
-  players: Player[],
-  coaches: Coach[]
+  teamName: string;
+  players: Player[];
+  coaches: Coach[];
 }
